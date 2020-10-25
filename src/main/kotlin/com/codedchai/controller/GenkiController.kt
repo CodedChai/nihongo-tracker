b7cb5f47d77f7a4ced66b8c39b77b29ae8b52d45
shaehamm <shaehamm@gmail.com>
2020-10-25 13:37:55 -0500

    Stubbed out a reminder endpoint with controller, service and domain object

---
+++ src/main/kotlin/com/codedchai/controller/GenkiController.kt
@@ -0,0 +1,21 @@
+package com.codedchai.controller
+
+import com.codedchai.domain.Reminder
+import com.codedchai.service.GenkiReminderService
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
+import kotlin.coroutines.CoroutineContext
+
+@Controller("v1/genki")
+class GenkiController(
+        val genkiReminderService: GenkiReminderService
+) {
+
+    @Get("/reminders")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getReminders(): List<Reminder> {
+        return genkiReminderService.getReminders()
+    }
+}
---
+++ src/main/kotlin/com/codedchai/domain/Reminder.kt
@@ -0,0 +1,9 @@
+package com.codedchai.domain
+
+import java.util.*
+
+data class Reminder (
+        val createdTimestamp: Date,
+        val chapterNumber : Int,
+        val reminderDate: Date
+)
