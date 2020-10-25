bee09021daa55013684e0dcea157ac445784e046
shaehamm <shaehamm@gmail.com>
2020-10-25 14:28:32 -0500

    cleaned up controller class

--- src/main/kotlin/com/codedchai/controller/GenkiController.kt
+++ src/main/kotlin/com/codedchai/controller/GenkiController.kt
@@ -6,9 +6,8 @@ import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
-import kotlin.coroutines.CoroutineContext
 
-@Controller("v1/genki")
+@Controller("/v1/genki")
 class GenkiController(
         val genkiReminderService: GenkiReminderService
 ) {
