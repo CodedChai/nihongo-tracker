b0b0304c9ac7e3f20d27384d2213d645d55dba6a
Connor Hasting <guardianx515@gmail.com>
2020-10-27 19:34:08 -0500

    added _id field

--- src/main/kotlin/com/codedchai/domain/DailyTask.kt
+++ src/main/kotlin/com/codedchai/domain/DailyTask.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat
 import java.util.*
 
 data class DailyTask (
+    val _id: String?,
     val pageNumber: Int,
     val chapterNumber : Int,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm a z")
