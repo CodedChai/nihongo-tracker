bfe4ba00c3f952031f5a6e01e4ee43ef05542202
shaehamm <shaehamm@gmail.com>
2020-10-31 11:55:08 -0500

    added some fake data and took out dependency to verify it works

--- src/main/kotlin/com/codedchai/service/GenkiReminderService.kt
+++ src/main/kotlin/com/codedchai/service/GenkiReminderService.kt
@@ -2,25 +2,35 @@ package com.codedchai.service
 
 import com.codedchai.domain.DailyTask
 import com.codedchai.domain.Reminder
-import com.codedchai.repository.TaskRepository
 import mu.KotlinLogging
 import java.util.*
 import javax.inject.Singleton
 
 @Singleton
-class GenkiReminderService(private val taskRepository: TaskRepository) {
+class GenkiReminderService() {//private val taskRepository: TaskRepository) {
 
-  private val logger = KotlinLogging.logger {}
+    private val logger = KotlinLogging.logger {}
 
-  fun getReminders(): List<Reminder> {
-    logger.debug { "getting genki reminders" }
-    return emptyList()
-  }
+    fun getReminders(): List<Reminder> {
+        logger.debug { "getting genki reminders" }
+        return emptyList()
+    }
 
-  fun getTasks(userName: String, startDate: Date, endDate: Date): List<DailyTask> {
-    // TODO: Query Mongo for all tasks between the given date range and user
-    logger.info { taskRepository.findItemsByUsername(userName) }
-    return emptyList()
-  }
+    fun getTasks(userName: String, startDate: Date, endDate: Date): List<DailyTask> {
+        // TODO: Query Mongo for all tasks between the given date range and user
+        // logger.info { taskRepository.findItemsByUsername(userName) }
+
+        // Make an example daily task in memory - data will be in database once working
+        // return it as a list
+        val dailyTask = DailyTask(
+                _id = null,
+                pageNumber = 10,
+                chapterNumber = 1,
+                dueDate = Date(),
+                isComplete = false,
+                userName = "Shae"
+        )
+        return listOf(dailyTask)
+    }
 
 }
