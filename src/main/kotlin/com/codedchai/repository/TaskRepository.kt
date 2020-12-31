da15fb2ef00e73d7140e32fa8c9a01e2fe2bfaf4
Connor Hasting <guardianx515@gmail.com>
2020-12-31 16:11:51 -0600

    Repository takes advantage of the coroutine client, we allow CORS for local Angular app

--- src/main/kotlin/com/codedchai/repository/TaskRepository.kt
+++ src/main/kotlin/com/codedchai/repository/TaskRepository.kt
@@ -2,6 +2,7 @@ package com.codedchai.repository
 
 import com.codedchai.domain.DailyTask
 import com.mongodb.client.model.Filters.eq
+import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
@@ -9,21 +10,29 @@ import javax.inject.Singleton
 
 @Singleton
 class TaskRepository() {
+
   private val logger = KotlinLogging.logger { }
 
-  private val client = KMongo.createClient("mongodb+srv://nihongo:<password>@cluster0.cb0gm.mongodb.net/NihongoTracker?retryWrites=true&w=majority").coroutine //use coroutine extension
+  private val client =
+    KMongo.createClient("mongodb+srv://nihongo:<pw>>@cluster0.cb0gm.mongodb.net/NihongoTracker?retryWrites=true&w=majority").coroutine
   private val database = client.getDatabase("NihongoTracker") //normal java driver usage
   private val collection = database.getCollection<DailyTask>("DailyTask") //KMongo extension method
 
   suspend fun findItemsByUsername(username: String): List<DailyTask?> {
     logger.info { "searching for username $username" }
-    return collection.find(
+
+    return runBlocking {
+      collection.find(
         eq(DailyTask::userName.name, username)
-    ).toList()
+      ).toList()
+    }
   }
 
   suspend fun save(dailyTask: DailyTask) {
     logger.info { "saving $dailyTask" }
-    collection.save(dailyTask)
+
+    runBlocking {
+      collection.save(dailyTask)
+    }
   }
 }
